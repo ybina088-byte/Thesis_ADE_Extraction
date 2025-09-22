e6f37e51061a11bd50c46ff957ef8ca7f446ab42
ybina088-byte <ybina088@gmail.com>
2025-09-22 13:38:58 +0100

    Update train_cadec_ner.py

--- src/train_cadec_ner.py
+++ src/train_cadec_ner.py
@@ -68,6 +68,7 @@ def compute_metrics(p):
 
 # ============== Training ======================
 args = TrainingArguments(
+training_args_kwargs = dict(
     output_dir="results/cadec_ner_biobert",
     eval_strategy="epoch",
     save_strategy="epoch",
@@ -82,8 +83,12 @@ args = TrainingArguments(
     greater_is_better=True,
 )
 
-args = TrainingArguments(**training_args_kwargs)
+if "evaluation_strategy" in TrainingArguments.__init__.__code__.co_varnames:
+    training_args_kwargs["evaluation_strategy"] = "epoch"
+else:
+    training_args_kwargs["eval_strategy"] = "epoch"
 
+args = TrainingArguments(**training_args_kwargs)
 
 data_collator = DataCollatorForTokenClassification(tokenizer)
 
